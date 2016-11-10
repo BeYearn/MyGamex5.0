507da357fe007fe8e659c01d158f707887f09dcc
BY <beyearn@gmail.com>
2016-11-10 17:02:18 +0800

    微博分享毛糙的接入成功（还未写入sdk中）

--- app/src/main/java/com/example/myggame/MainActivity.java
+++ app/src/main/java/com/example/myggame/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myggame;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -8,7 +11,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.emagroup.sdk.EmaBackPressedAction;
@@ -18,54 +20,73 @@ import com.emagroup.sdk.EmaSDK;
 import com.emagroup.sdk.EmaSDKListener;
 import com.emagroup.sdk.EmaUser;
 import com.emagroup.sdk.ToastHelper;
+import com.sina.weibo.sdk.api.ImageObject;
+import com.sina.weibo.sdk.api.TextObject;
+import com.sina.weibo.sdk.api.WeiboMultiMessage;
+import com.sina.weibo.sdk.api.share.BaseResponse;
+import com.sina.weibo.sdk.api.share.IWeiboHandler;
+import com.sina.weibo.sdk.api.share.IWeiboShareAPI;
+import com.sina.weibo.sdk.api.share.SendMultiMessageToWeiboRequest;
+import com.sina.weibo.sdk.api.share.WeiboShareSDK;
+import com.sina.weibo.sdk.constant.WBConstants;
 
 import java.util.HashMap;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener, IWeiboHandler.Response {
 
     private Button btLogin;
     protected boolean isSuccess;
     private Handler uiHandler;
     private Button btPay;
     private LinearLayout myLayout;
-    private TextView tvName;
     private Button btLogout;
     private Button btShowBar;
     private Button btHideBar;
     private Button btSwichAccount;
+    private IWeiboShareAPI mWeiboShareAPI;
+    private Button btShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // 创建微博分享接口实例-----------------------------------------------------------------------------------
+        mWeiboShareAPI = WeiboShareSDK.createWeiboAPI(this, "721964606");
+        mWeiboShareAPI.registerApp();
+
+
+        //微博-------------------------------------------------------------------------------------
+
+
         uiHandler = new Handler();
-        tvName = (TextView) findViewById(R.id.tv_login);
         btLogin = (Button) findViewById(R.id.bt_login);
         btPay = (Button) findViewById(R.id.bt_pay);
-        btLogout= (Button) findViewById(R.id.bt_logout);
-        btShowBar= (Button) findViewById(R.id.bt_showbar);
-        btHideBar= (Button) findViewById(R.id.bt_hidebar);
-        btSwichAccount= (Button) findViewById(R.id.bt_swichaccount);
+        btLogout = (Button) findViewById(R.id.bt_logout);
+        btShowBar = (Button) findViewById(R.id.bt_showbar);
+        btHideBar = (Button) findViewById(R.id.bt_hidebar);
+        btSwichAccount = (Button) findViewById(R.id.bt_swichaccount);
+        btShare = (Button) findViewById(R.id.bt_share);
 
 
-        EmaSDK.getInstance().init("6cdd60ea0045eb7a6ec44c54d29ed402",this, new EmaSDKListener() {
-        //EmaSDK.getInstance().init("5600441101c8818c4480d3c503742a3b",this, new EmaSDKListener() {
-        //EmaSDK.getInstance().init("800a924c499772bac7b76432803ea47a",this, new EmaSDKListener() {  //10001
+        EmaSDK.getInstance().init("6cdd60ea0045eb7a6ec44c54d29ed402", this, new EmaSDKListener() {
+            //EmaSDK.getInstance().init("5600441101c8818c4480d3c503742a3b",this, new EmaSDKListener() {
+            //EmaSDK.getInstance().init("800a924c499772bac7b76432803ea47a",this, new EmaSDKListener() {  //10001
             @Override
             public void onCallBack(int arg0, String arg1) {
-                Log.e("mainactivity",arg0+"++++++++++++++++ "+arg1);
+                Log.e("mainactivity", arg0 + "++++++++++++++++ " + arg1);
                 switch (arg0) {
                     case EmaCallBackConst.INITSUCCESS://初始化SDK成功回调
                         isSuccess = true;
-                        Log.e("Mainactivity","sdk初始化成功");
+                        Log.e("Mainactivity", "sdk初始化成功");
                         break;
                     case EmaCallBackConst.INITFALIED://初始化SDK失败回调
-                        Log.e("Mainactivity","sdk初始化失败");
+                        Log.e("Mainactivity", "sdk初始化失败");
                         break;
                     case EmaCallBackConst.LOGINSUCCESS://登陆成功回调
-                        ToastHelper.toast(MainActivity.this,"登陆成功");
-                        Log.e("Mainactivity",EmaUser.getInstance().getNickName());
+                        ToastHelper.toast(MainActivity.this, "登陆成功");
+                        Log.e("Mainactivity", EmaUser.getInstance().getNickName());
                         Log.e("Mainactivity", EmaUser.getInstance().getAllianceUid());
                         Log.e("Mainactivity", EmaSDK.getInstance().getChannelId());
                         break;
@@ -73,14 +94,14 @@ public class MainActivity extends Activity implements OnClickListener {
                         break;
                     case EmaCallBackConst.LOGINFALIED://登陆失败回调
                         Log.e("++++++++++", Thread.currentThread().getName());
-                        ToastHelper.toast(MainActivity.this,"登陆失败");
+                        ToastHelper.toast(MainActivity.this, "登陆失败");
                         break;
                     case EmaCallBackConst.LOGOUTSUCCESS://登出成功回调
                         break;
                     case EmaCallBackConst.LOGOUTFALIED://登出失败回调
                         break;
                     case EmaCallBackConst.ACCOUNTSWITCHSUCCESS:
-                        Log.e("EMASDK","ACCOUNTSWITCHSUCCESS");//切换帐号成功回调
+                        Log.e("EMASDK", "ACCOUNTSWITCHSUCCESS");//切换帐号成功回调
                         break;
                     case EmaCallBackConst.ACCOUNTSWITCHFAIL:
                         break;
@@ -91,31 +112,113 @@ public class MainActivity extends Activity implements OnClickListener {
         EmaSDK.getInstance().doSetRecivePushListner(new EmaSDKListener() {
             @Override
             public void onCallBack(int resultCode, String data) {
-                if(resultCode== EmaCallBackConst.RECIVEMSG_MSG){
+                if (resultCode == EmaCallBackConst.RECIVEMSG_MSG) {
                     // TODO:  data为拿到的推送数据,自行处理
-                    Toast.makeText(MainActivity.this,data, Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
                 }
             }
         });
         //initPayListner();
 
-        tvName.setOnClickListener(this);
         btLogin.setOnClickListener(this);
         btPay.setOnClickListener(this);
         btLogout.setOnClickListener(this);
         btShowBar.setOnClickListener(this);
         btHideBar.setOnClickListener(this);
         btSwichAccount.setOnClickListener(this);
+        btShare.setOnClickListener(this);
 
         Log.e("++++++++++", Thread.currentThread().getName());
     }
 
     @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        // 从当前应用唤起微博并进行分享后，返回到当前应用时，需要在此处调用该函数
+        // 来接收微博客户端返回的数据；执行成功，返回 true，并调用
+        // {@link IWeiboHandler.Response#onResponse}；失败返回 false，不调用上述回调
+        /*mWeiboShareAPI.handleWeiboResponse(intent, new IWeiboHandler.Response() {
+            @Override
+            public void onResponse(BaseResponse baseResponse) {
+                if (baseResponse != null) {
+                    Log.e("weibofenxiang",baseResponse.toString());
+                    switch (baseResponse.errCode) {
+                        case WBConstants.ErrorCode.ERR_OK:
+                            ToastHelper.toast(MainActivity.this, "share successful");
+                            break;
+                        case WBConstants.ErrorCode.ERR_CANCEL:
+                            ToastHelper.toast(MainActivity.this, "share cancel");
+                            break;
+                        case WBConstants.ErrorCode.ERR_FAIL:
+                            ToastHelper.toast(MainActivity.this, "share fail");
+                            break;
+                    }
+                }
+            }
+        });*/
+        mWeiboShareAPI.handleWeiboResponse(intent, this);
+    }
+
+    @Override
+    public void onResponse(BaseResponse baseResponse) {
+        if (baseResponse != null) {
+            Log.e("weibofenxiang", baseResponse.toString());
+            switch (baseResponse.errCode) {
+                case WBConstants.ErrorCode.ERR_OK:
+                    ToastHelper.toast(MainActivity.this, "share successful");
+                    break;
+                case WBConstants.ErrorCode.ERR_CANCEL:
+                    ToastHelper.toast(MainActivity.this, "share cancel");
+                    break;
+                case WBConstants.ErrorCode.ERR_FAIL:
+                    ToastHelper.toast(MainActivity.this, "share fail");
+                    break;
+            }
+        }
+    }
+    //微博-------------------------------------------------------------------------------------
+    private void doShare() {
+        // 1. 初始化微博的分享消息
+        WeiboMultiMessage weiboMessage = new WeiboMultiMessage();
+        weiboMessage.textObject = getTextObj();
+        weiboMessage.imageObject = getImageObj();
+
+        // 2. 初始化从第三方到微博的消息请求
+        SendMultiMessageToWeiboRequest request = new SendMultiMessageToWeiboRequest();
+        // 用transaction唯一标识一个请求
+        request.transaction = String.valueOf(System.currentTimeMillis());
+        request.multiMessage = weiboMessage;
+
+        // 3. 发送请求消息到微博，唤起微博分享界面
+        mWeiboShareAPI.sendRequest(this, request);
+    }
+
+    private ImageObject getImageObj() {
+        ImageObject imageObject = new ImageObject();
+        //BitmapDrawable bitmapDrawable = (BitmapDrawable) mImageView.getDrawable();
+        //设置缩略图。 注意：最终压缩过的缩略图大小不得超过 32kb。
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ema_floating_icon);
+        imageObject.setImageObject(bitmap);
+        return imageObject;
+    }
+
+    /**
+     * 创建文本消息对象。
+     *
+     * @return 文本消息对象。
+     */
+    private TextObject getTextObj() {
+        TextObject textObject = new TextObject();
+        textObject.text = "我是微信分享模板";
+        return textObject;
+    }
+
+    //微博-------------------------------------------------------------------------------------
+
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.tv_login:
-
-                break;
             case R.id.bt_login:
                 MainActivity.this.runOnUiThread(  //有的接入会发生不在主线程调用该方法，所以最外面加上这个
                         new Runnable() {
@@ -145,7 +248,7 @@ public class MainActivity extends Activity implements OnClickListener {
                 HashMap<String, String> payInfoMap = new HashMap<>();
                 payInfoMap.put(EmaConst.EMA_PAYINFO_PRODUCT_ID, "10001");
                 payInfoMap.put(EmaConst.EMA_PAYINFO_PRODUCT_COUNT, "1");
-                payInfoMap.put(EmaConst.EMA_GAMETRANS_CODE,"游戏透传参数");
+                payInfoMap.put(EmaConst.EMA_GAMETRANS_CODE, "游戏透传参数");
                 //payInfoMap.put("Product_Name","gold");
                 //payInfoMap.put("Server_Id", "13");
                 //payInfoMap.put("Role_Id","1");
@@ -159,21 +262,24 @@ public class MainActivity extends Activity implements OnClickListener {
                         Log.d(String.valueOf(arg0), arg1);
                         switch (arg0) {
                             case EmaCallBackConst.PAYSUCCESS:// 支付成功回调
-                                ToastHelper.toast(MainActivity.this,"pay successful---");
+                                ToastHelper.toast(MainActivity.this, "pay successful---");
                                 break;
                             case EmaCallBackConst.PAYFALIED:// 支付失败回调
-                                ToastHelper.toast(MainActivity.this,"pay failed---");
+                                ToastHelper.toast(MainActivity.this, "pay failed---");
                                 break;
                             case EmaCallBackConst.PAYCANELI:// 支付取消回调
-                                ToastHelper.toast(MainActivity.this,"pay Cancel");
+                                ToastHelper.toast(MainActivity.this, "pay Cancel");
                                 break;
                         }
                     }
                 });
                 break;
             case R.id.bt_swichaccount:
-                Log.e("bt_swichaccount","dianjile ");
+                Log.e("bt_swichaccount", "dianjile ");
                 EmaSDK.getInstance().doSwichAccount();
+                break;
+            case R.id.bt_share:
+                doShare();
                 break;
         }
 
@@ -240,14 +346,15 @@ public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     public void onBackPressed() {
-        EmaSDK.getInstance().onBackPressed(new EmaBackPressedAction(){
+        EmaSDK.getInstance().onBackPressed(new EmaBackPressedAction() {
             @Override
             public void doBackPressedAction() {
                 //游戏需要做的逻辑
-                ToastHelper.toast(MainActivity.this,"我是游戏自己的逻辑xxxxxxxxxxxxx");
+                ToastHelper.toast(MainActivity.this, "我是游戏自己的逻辑xxxxxxxxxxxxx");
                 MainActivity.super.onBackPressed();
             }
         });
         //super.onBackPressed();
     }
+
 }
