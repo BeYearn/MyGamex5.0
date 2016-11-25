f7fccc4fb9bfe0d92217f30e50a07ab01e435bf4
yanpeng <503424551@qq.com>
2016-11-25 17:05:00 +0800

    视频 音频 微信分享

--- emasdklib/src/main/java/com/emagroup/sdk/WeixinShareUtils.java
+++ emasdklib/src/main/java/com/emagroup/sdk/WeixinShareUtils.java
@@ -3,17 +3,21 @@ package com.emagroup.sdk;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 
 import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
 import com.tencent.mm.sdk.modelmsg.WXImageObject;
 import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.sdk.modelmsg.WXMusicObject;
 import com.tencent.mm.sdk.modelmsg.WXTextObject;
+import com.tencent.mm.sdk.modelmsg.WXVideoObject;
 import com.tencent.mm.sdk.modelmsg.WXWebpageObject;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import cn.emagroup.sdk.R;
@@ -26,6 +30,9 @@ public class WeixinShareUtils {
     private final Activity mActivity;
     private final IWXAPI mWeixinapi;
     public static EmaSDKListener mListener;
+    private static final int MMAlertSelect1  =  0;
+    private static final int MMAlertSelect2  =  1;
+    private static final int MMAlertSelect3  =  2;
 
     public static WeixinShareUtils getInstance(Activity activity) {
         if (mInstance == null) {
@@ -43,7 +50,11 @@ public class WeixinShareUtils {
 
     public void doWeixinShare(EmaSDKListener listener) {
         this.mListener=listener;
-        doWxShareText();
+        // doWxShareText();
+        // doWxShareMusic(MMAlertSelect2);
+        //   doWxShareVideo(MMAlertSelect2);
+        //   doWxShareImg1(MMAlertSelect3);
+         doWxShareImg3();
     }
 
 
@@ -65,7 +76,60 @@ public class WeixinShareUtils {
         mWeixinapi.sendReq(req);
 
     }
-    private void doWxShareImg1(){
+    private void doWxShareImg1( int type ){
+        /*WXMediaMessage msg=null;
+        switch (type){
+            case MMAlertSelect1:
+                Bitmap bmp = BitmapFactory.decodeResource(mActivity.getResources(), R.drawable.ema_floating_icon);
+                WXImageObject imgObj = new WXImageObject(bmp);
+
+               msg = new WXMediaMessage();
+                msg.mediaObject = imgObj;
+
+                Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+                bmp.recycle();
+                msg.thumbData = bmpToByteArray(thumbBmp, true);  // 设置缩略图
+                break;
+            case MMAlertSelect2:
+                String path = "storage/emulated/0/DCIM/Camera"+ "/cup.jpg";
+                File file = new File(path);
+                if (!file.exists()) {
+                    ToastHelper.toast(mActivity,"请检查路径");
+                    return;
+                }
+
+                 imgObj = new WXImageObject();
+                imgObj.setImagePath(path);
+
+                  msg = new WXMediaMessage();
+                msg.mediaObject = imgObj;
+
+                 bmp = BitmapFactory.decodeFile(path);
+                  thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+                bmp.recycle();
+                msg.thumbData = bmpToByteArray(thumbBmp, true);
+
+                break;
+            case MMAlertSelect3:
+                String url = "http://pic14.nipic.com/20110524/1365113_101909990000_2.jpg";
+
+
+                imgObj = new WXImageObject();
+                //imgObj.imageUrl=url;
+
+                msg = new WXMediaMessage();
+                msg.mediaObject = imgObj;
+
+                try {bmp = BitmapFactory.decodeStream(new URL(url).openStream());
+                thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+                bmp.recycle();
+                msg.thumbData = bmpToByteArray(thumbBmp, true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+      sendReq(msg);*/
         Bitmap bmp = BitmapFactory.decodeResource(mActivity.getResources(), R.drawable.ema_floating_icon);
         WXImageObject imgObj = new WXImageObject(bmp);
 
@@ -82,7 +146,7 @@ public class WeixinShareUtils {
         req.scene = SendMessageToWX.Req.WXSceneSession;// 或者SendMessageToWX.Req.WXSceneTimeline
         mWeixinapi.sendReq(req);
     }
-    private void doWxShareImg2(){
+     private void doWxShareImg2(){
         //String path = SDCARD_ROOT + "/test.png";
         String path = "storage/emulated/0/DCIM/Camera"+ "/cup.jpg";
         File file = new File(path);
@@ -110,17 +174,63 @@ public class WeixinShareUtils {
     }
 
     private void doWxShareImg3(){
-        String url = "http://weixin.qq.com/zh_CN/htmledition/images/weixin/weixin_logo0d1938.png";
+        final String url = "http://pic14.nipic.com/20110524/1365113_101909990000_2.jpg";
 
         try{
             WXImageObject imgObj = new WXImageObject();
-            //imgObj.imageUrl=url;
+            imgObj.imageUrl=url;
 
-            WXMediaMessage msg = new WXMediaMessage();
+            final WXMediaMessage msg = new WXMediaMessage();
             msg.mediaObject = imgObj;
 
-            Bitmap bmp = BitmapFactory.decodeStream(new URL(url).openStream());
-            Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+           /* new AsyncTask<String, Void, Bitmap>() {
+                @Override
+                protected Bitmap doInBackground(String... strings) {
+                    Bitmap bmp = null;
+                    try {
+                        bmp  = BitmapFactory.decodeStream(new URL(strings[0]).openStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return  bmp;
+                }
+
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    Bitmap thumbBmp = Bitmap.createScaledBitmap(bitmap, 150, 150, true);
+                    bitmap.recycle();
+                    msg.thumbData = bmpToByteArray(thumbBmp, true);
+
+                    SendMessageToWX.Req req = new SendMessageToWX.Req();
+                    req.transaction = String.valueOf(System.currentTimeMillis()); // transaction字段用于唯一标识一个请求
+                    req.message = msg;
+                    req.scene = SendMessageToWX.Req.WXSceneSession;// 或者SendMessageToWX.Req.WXSceneTimeline
+                    mWeixinapi.sendReq(req);
+                }
+            }.execute(url);*/
+
+            new Thread() {
+                public void run() {
+                    Bitmap bmp = null;
+                    try {
+                        bmp = BitmapFactory.decodeStream(new URL(url).openStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+                    bmp.recycle();
+                    msg.thumbData = bmpToByteArray(thumbBmp, true);
+
+                    SendMessageToWX.Req req = new SendMessageToWX.Req();
+                    req.transaction = String.valueOf(System.currentTimeMillis()); // transaction字段用于唯一标识一个请求
+                    req.message = msg;
+                    req.scene = SendMessageToWX.Req.WXSceneSession;// 或者SendMessageToWX.Req.WXSceneTimeline
+                    mWeixinapi.sendReq(req);
+                }
+            }.start();
+
+           // Bitmap bmp = BitmapFactory.decodeStream(new URL(url).openStream());
+          /*  Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
             bmp.recycle();
             msg.thumbData = bmpToByteArray(thumbBmp, true);
 
@@ -128,7 +238,7 @@ public class WeixinShareUtils {
             req.transaction = String.valueOf(System.currentTimeMillis()); // transaction字段用于唯一标识一个请求
             req.message = msg;
             req.scene = SendMessageToWX.Req.WXSceneSession;// 或者SendMessageToWX.Req.WXSceneTimeline
-            mWeixinapi.sendReq(req);
+            mWeixinapi.sendReq(req);*/
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -150,6 +260,75 @@ public class WeixinShareUtils {
         req.scene = SendMessageToWX.Req.WXSceneSession;// 或者SendMessageToWX.Req.WXSceneTimeline
         mWeixinapi.sendReq(req);
     }
+
+    private void doWxShareMusic(int type){
+        WXMediaMessage msg = null;
+
+        switch (type){
+            case MMAlertSelect1:
+                WXMusicObject music = new WXMusicObject();
+                music.musicUrl="http://ok.96x.cn/2014/ting30_chang30_4yue/%E4%BA%B2%E7%88%B1%E7%9A%84%E6%88%91%E7%88%B1%E4%BD%A0%20%E7%A5%81%E9%9A%86.mp3";
+
+                msg = new WXMediaMessage();
+                msg.mediaObject = music;
+                msg.title = "Music Title Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long";
+                msg.description = "Music Album Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long";
+
+                Bitmap thumb = BitmapFactory.decodeResource(mActivity.getResources(), R.drawable.ic_launcher);
+                msg.thumbData = bmpToByteArray(thumb, true);
+                break;
+            case MMAlertSelect2:
+                 music = new WXMusicObject();
+                music.musicLowBandUrl = "http://www.qq.com";
+
+                 msg = new WXMediaMessage();
+                msg.mediaObject = music;
+                msg.title = "Music Title";
+                msg.description = "Music Album";
+
+                thumb = BitmapFactory.decodeResource(mActivity.getResources(), R.drawable.ic_launcher);
+                msg.thumbData = bmpToByteArray(thumb, true);
+                break;
+        }
+        sendReq(msg);
+    }
+
+    private void doWxShareVideo(int type){
+        WXMediaMessage msg = null;
+
+        switch (type){
+            case MMAlertSelect1:
+                WXVideoObject video = new WXVideoObject();
+                video.videoUrl = "http://www.baidu.com";
+
+                msg = new WXMediaMessage(video);
+                msg.title = "Video Title Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long";
+                msg.description = "Video Description Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long";
+                Bitmap thumb = BitmapFactory.decodeResource(mActivity.getResources(), R.drawable.ic_launcher);
+                msg.thumbData = bmpToByteArray(thumb, true);
+
+                break;
+            case MMAlertSelect2:
+                video = new WXVideoObject();
+                video.videoLowBandUrl = "http://www.qq.com";
+
+                msg = new WXMediaMessage(video);
+                msg.title = "Video Title";
+                msg.description = "Video Description";
+                break;
+
+        }
+        sendReq(msg);
+    }
+
+    private void sendReq(WXMediaMessage msg) {
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = String.valueOf(System.currentTimeMillis()); // transaction字段用于唯一标识一个请求
+        req.message = msg;
+        req.scene = SendMessageToWX.Req.WXSceneSession;// 或者SendMessageToWX.Req.WXSceneTimeline
+        mWeixinapi.sendReq(req);
+    }
+
 
     public static byte[] bmpToByteArray(final Bitmap bmp, final boolean needRecycle) {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
